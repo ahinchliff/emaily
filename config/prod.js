ae623d62681f9dadd7fc54853467510f3fc59c98
ahinchliff <anthony.hinchliff@gmail.com>
2017-08-25 22:36:22 +1000

    google strategy settings

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-  googleClientID: '473840088710-iijt9e0pg8hlvvq3nr03s2f3q15c710d.apps.googleusercontent.com',
-  googleClientSecret: 'gxrDdGjF73QSNQ0gAIZ0XS2x',
-  mongoURI: 'mongodb://admin_user:password@ds159013.mlab.com:59013/emaily-dev',
-  cookieKey: "wq28347932498214790128fnsdfhg4h"
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
 };
